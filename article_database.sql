70a737cab850284eda5e3b18f09d2d1c9e413e8d
kevinash92 <esselebomebara@gmail.com>
2019-04-15 02:53:11 +0100

    Authenticating Clients against the credentials stored in the database

--- article_database.sql
+++ article_database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Client :  localhost:3306
--- Généré le :  Lun 08 Avril 2019 à 01:21
+-- Généré le :  Lun 15 Avril 2019 à 02:50
 -- Version du serveur :  10.1.26-MariaDB-0+deb9u1
 -- Version de PHP :  7.0.30-0+deb9u1
 
@@ -30,7 +30,11 @@ CREATE TABLE `account` (
   `id` bigint(20) NOT NULL,
   `login` varchar(20) NOT NULL,
   `password` varchar(64) NOT NULL,
-  `email` varchar(20) NOT NULL,
+  `email` varchar(64) NOT NULL,
+  `non_expired` tinyint(1) NOT NULL DEFAULT '1',
+  `non_locked` tinyint(1) NOT NULL DEFAULT '1',
+  `credentials_non_expired` tinyint(1) NOT NULL DEFAULT '1',
+  `enabled` tinyint(1) NOT NULL DEFAULT '1',
   `version` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -38,10 +42,11 @@ CREATE TABLE `account` (
 -- Contenu de la table `account`
 --
 
-INSERT INTO `account` (`id`, `login`, `password`, `email`, `version`) VALUES
-(7, 'admin', '$2a$10$wxxgtQMZRnjIoSfeo7f3gejQQNWIQebWYRIYGakvlR.4fMMWOHIQq', 'admin@test.com', 0),
-(8, 'user', '$2a$10$gIFxo.hIsXzyErLqExUQsu.WDJslMXjRa5/BJPkhKJHWfY50U/Una', 'user@test.com', 0),
-(9, 'guest', '$2a$10$AzAHRCilc.jBOJdIuxpWOu66DToXX4r3CqzX0XGrye5w9IaBG4k9y', 'guest@email.com', 0);
+INSERT INTO `account` (`id`, `login`, `password`, `email`, `non_expired`, `non_locked`, `credentials_non_expired`, `enabled`, `version`) VALUES
+(7, 'admin', '$2a$10$wxxgtQMZRnjIoSfeo7f3gejQQNWIQebWYRIYGakvlR.4fMMWOHIQq', 'admin@test.com', 0, 0, 0, 0, 0),
+(8, 'user', '$2a$10$gIFxo.hIsXzyErLqExUQsu.WDJslMXjRa5/BJPkhKJHWfY50U/Una', 'user@test.com', 1, 1, 1, 1, 0),
+(9, 'guest', '$2a$10$AzAHRCilc.jBOJdIuxpWOu66DToXX4r3CqzX0XGrye5w9IaBG4k9y', 'guest@email.com', 1, 1, 1, 1, 0),
+(12, 'esselebo', '$2a$10$TW.9BmbN2S64LvKAbVYr6ujoqwslknGWnI4znLCwsiJWGfzBx7Emy', 't@test.com', 1, 1, 1, 1, 0);
 
 -- --------------------------------------------------------
 
@@ -63,7 +68,9 @@ CREATE TABLE `account_role` (
 INSERT INTO `account_role` (`id`, `id_account`, `id_role`, `version`) VALUES
 (4, 7, 4, 0),
 (5, 8, 5, 0),
-(6, 9, 6, 0);
+(6, 9, 6, 0),
+(7, 12, 4, 0),
+(8, 12, 5, 0);
 
 -- --------------------------------------------------------
 
@@ -102,6 +109,7 @@ DELIMITER ;
 
 CREATE TABLE `author` (
   `id` bigint(20) NOT NULL,
+  `id_account` bigint(20) NOT NULL,
   `firstname` varchar(255) DEFAULT NULL,
   `lastname` varchar(255) DEFAULT NULL,
   `birthday` date DEFAULT NULL,
@@ -114,8 +122,103 @@ CREATE TABLE `author` (
 -- Contenu de la table `author`
 --
 
-INSERT INTO `author` (`id`, `firstname`, `lastname`, `birthday`, `address`, `sex`, `version`) VALUES
-(4, 'ESSELEBO', 'Kevin', '2019-04-07', 'Boutsoungoulou, City', 'homme', 0);
+INSERT INTO `author` (`id`, `id_account`, `firstname`, `lastname`, `birthday`, `address`, `sex`, `version`) VALUES
+(4, 7, 'ESSELEBO', 'Kevin', '2019-04-07', 'Boutsoungoulou, City', 'homme', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `oauth_access_token`
+--
+
+CREATE TABLE `oauth_access_token` (
+  `token_id` varchar(256) DEFAULT NULL,
+  `token` varchar(256) DEFAULT NULL,
+  `authentication_id` varchar(128) NOT NULL,
+  `user_name` varchar(256) DEFAULT NULL,
+  `client_id` varchar(256) DEFAULT NULL,
+  `authentication` varchar(256) DEFAULT NULL,
+  `refresh_token` varchar(256) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `oauth_approvals`
+--
+
+CREATE TABLE `oauth_approvals` (
+  `userId` varchar(256) DEFAULT NULL,
+  `clientId` varchar(256) DEFAULT NULL,
+  `scope` varchar(256) DEFAULT NULL,
+  `status` varchar(10) DEFAULT NULL,
+  `expiresAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `lastModifiedAt` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `oauth_client_details`
+--
+
+CREATE TABLE `oauth_client_details` (
+  `client_id` varchar(128) NOT NULL,
+  `resource_ids` varchar(256) DEFAULT NULL,
+  `client_secret` varchar(256) DEFAULT NULL,
+  `scope` varchar(256) DEFAULT NULL,
+  `authorized_grant_types` varchar(256) DEFAULT NULL,
+  `web_server_redirect_uri` varchar(256) DEFAULT NULL,
+  `authorities` varchar(256) DEFAULT NULL,
+  `access_token_validity` int(11) DEFAULT NULL,
+  `refresh_token_validity` int(11) DEFAULT NULL,
+  `additional_information` varchar(4096) DEFAULT NULL,
+  `autoapprove` varchar(256) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Contenu de la table `oauth_client_details`
+--
+
+INSERT INTO `oauth_client_details` (`client_id`, `resource_ids`, `client_secret`, `scope`, `authorized_grant_types`, `web_server_redirect_uri`, `authorities`, `access_token_validity`, `refresh_token_validity`, `additional_information`, `autoapprove`) VALUES
+('crmClient1', NULL, '$2a$10$QX9U6lHw5TQ4Juoj1bhBN.3l9PCXPpaBxt9cXuWRaUI/fH9mv9lp6', 'read,write,trust', 'password,refresh_token', NULL, 'ROLE_CLIENT,ROLE_TRUSTED_CLIENT', 900, 2592000, NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `oauth_client_token`
+--
+
+CREATE TABLE `oauth_client_token` (
+  `token_id` varchar(256) DEFAULT NULL,
+  `token` varchar(256) DEFAULT NULL,
+  `authentication_id` varchar(128) NOT NULL,
+  `user_name` varchar(256) DEFAULT NULL,
+  `client_id` varchar(256) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `oauth_code`
+--
+
+CREATE TABLE `oauth_code` (
+  `code` varchar(256) DEFAULT NULL,
+  `authentication` varchar(256) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `oauth_refresh_token`
+--
+
+CREATE TABLE `oauth_refresh_token` (
+  `token_id` varchar(256) DEFAULT NULL,
+  `token` varchar(256) DEFAULT NULL,
+  `authentication` varchar(256) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -171,7 +274,26 @@ ALTER TABLE `article`
 -- Index pour la table `author`
 --
 ALTER TABLE `author`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id_account` (`id_account`);
+
+--
+-- Index pour la table `oauth_access_token`
+--
+ALTER TABLE `oauth_access_token`
+  ADD PRIMARY KEY (`authentication_id`);
+
+--
+-- Index pour la table `oauth_client_details`
+--
+ALTER TABLE `oauth_client_details`
+  ADD PRIMARY KEY (`client_id`);
+
+--
+-- Index pour la table `oauth_client_token`
+--
+ALTER TABLE `oauth_client_token`
+  ADD PRIMARY KEY (`authentication_id`);
 
 --
 -- Index pour la table `role`
@@ -188,17 +310,17 @@ ALTER TABLE `role`
 -- AUTO_INCREMENT pour la table `account`
 --
 ALTER TABLE `account`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 --
 -- AUTO_INCREMENT pour la table `account_role`
 --
 ALTER TABLE `account_role`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 --
 -- AUTO_INCREMENT pour la table `article`
 --
 ALTER TABLE `article`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT pour la table `author`
 --
@@ -225,6 +347,12 @@ ALTER TABLE `account_role`
 --
 ALTER TABLE `article`
   ADD CONSTRAINT `article_ibfk_1` FOREIGN KEY (`id_author`) REFERENCES `author` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `author`
+--
+ALTER TABLE `author`
+  ADD CONSTRAINT `author_ibfk_1` FOREIGN KEY (`id_account`) REFERENCES `account` (`id`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
